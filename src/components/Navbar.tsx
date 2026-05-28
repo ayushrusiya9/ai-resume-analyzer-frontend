2f7fc267fc8254923e13e7ff6718eb853e2837d1
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-05-28 23:44:32 +0530

    feat: update tailwind css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     };
     return (
         <nav className="w-full">
-            <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between bg-white rounded-full shadow-2xl px-4 py-3 sm:px-6 sm:py-4 mt-4">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between bg-white rounded-full shadow-2xl px-4 py-3 sm:px-6 sm:py-4 mt-4">
                 <h1 className="lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold bg-linear-to-r from-[#AB8C95] via-[#000000] to-[#8E97C5] bg-clip-text text-transparent ">
                     ANALYZE
                 </h1>
